dd7d38a31ffe11e22f6b81ce8d25f821d65ef27a
Ralitsa Ivanova <rivanova@appolica.com>
2017-03-02 13:05:23 +0200

    Fix: Optionals crash Add: Key before value

--- GMStepper/GMStepper.swift
+++ GMStepper/GMStepper.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 @IBDesignable public class GMStepper: UIControl {
-
+    
+    @IBInspectable public var keyBeforeValue: String = "£"
+    
     /// Current value of the stepper. Defaults to 0.
     @IBInspectable public var value: Double = 0 {
         didSet {
             value = min(maximumValue, max(minimumValue, value))
-
+            
             let isInteger = floor(value) == value
             
             //
@@ -25,54 +27,54 @@ import UIKit
                 label.text = items[Int(value)]
             }
             else if showIntegerIfDoubleIsInteger && isInteger {
-                label.text = String(stringInterpolationSegment: Int(value))
+                label.text = "\(keyBeforeValue)\(String(stringInterpolationSegment: Int(value)))"
             } else {
-                label.text = String(stringInterpolationSegment: value)
+                label.text = "\(keyBeforeValue)\(String(stringInterpolationSegment: value))"
             }
-
+            
             if oldValue != value {
                 sendActions(for: .valueChanged)
             }
         }
     }
-
+    
     /// Minimum value. Must be less than maximumValue. Defaults to 0.
     @IBInspectable public var minimumValue: Double = 0 {
         didSet {
             value = min(maximumValue, max(minimumValue, value))
         }
     }
-
+    
     /// Maximum value. Must be more than minimumValue. Defaults to 100.
     @IBInspectable public var maximumValue: Double = 100 {
         didSet {
             value = min(maximumValue, max(minimumValue, value))
         }
     }
-
+    
     /// Step/Increment value as in UIStepper. Defaults to 1.
     @IBInspectable public var stepValue: Double = 1
-
+    
     /// The same as UIStepper's autorepeat. If true, holding on the buttons or keeping the pan gesture alters the value repeatedly. Defaults to true.
     @IBInspectable public var autorepeat: Bool = true
-
+    
     /// If the value is integer, it is shown without floating point.
     @IBInspectable public var showIntegerIfDoubleIsInteger: Bool = true
-
+    
     /// Text on the left button. Be sure that it fits in the button. Defaults to "−".
     @IBInspectable public var leftButtonText: String = "−" {
         didSet {
             leftButton.setTitle(leftButtonText, for: .normal)
         }
     }
-
+    
     /// Text on the right button. Be sure that it fits in the button. Defaults to "+".
     @IBInspectable public var rightButtonText: String = "+" {
         didSet {
             rightButton.setTitle(rightButtonText, for: .normal)
         }
     }
-
+    
     /// Text color of the buttons. Defaults to white.
     @IBInspectable public var buttonsTextColor: UIColor = UIColor.white {
         didSet {
@@ -81,7 +83,7 @@ import UIKit
             }
         }
     }
-
+    
     /// Background color of the buttons. Defaults to dark blue.
     @IBInspectable public var buttonsBackgroundColor: UIColor = UIColor(red:0.21, green:0.5, blue:0.74, alpha:1) {
         didSet {
@@ -91,37 +93,37 @@ import UIKit
             backgroundColor = buttonsBackgroundColor
         }
     }
-
+    
     /// Font of the buttons. Defaults to AvenirNext-Bold, 20.0 points in size.
-    public var buttonsFont = UIFont(name: "AvenirNext-Bold", size: 20.0)! {
+    public var buttonsFont = UIFont(name: "HelveticaNeue-Medium", size: 32.0)! {
         didSet {
             for button in [leftButton, rightButton] {
                 button.titleLabel?.font = buttonsFont
             }
         }
     }
-
+    
     /// Text color of the middle label. Defaults to white.
     @IBInspectable public var labelTextColor: UIColor = UIColor.white {
         didSet {
             label.textColor = labelTextColor
         }
     }
-
+    
     /// Text color of the middle label. Defaults to lighter blue.
     @IBInspectable public var labelBackgroundColor: UIColor = UIColor(red:0.26, green:0.6, blue:0.87, alpha:1) {
         didSet {
             label.backgroundColor = labelBackgroundColor
         }
     }
-
+    
     /// Font of the middle label. Defaults to AvenirNext-Bold, 25.0 points in size.
-    public var labelFont = UIFont(name: "AvenirNext-Bold", size: 25.0)! {
+    public var labelFont = UIFont(name: "HelveticaNeue-Medium", size: 20.0)! {
         didSet {
             label.font = labelFont
         }
     }
-
+    
     /// Corner radius of the stepper's layer. Defaults to 4.0.
     @IBInspectable public var cornerRadius: CGFloat = 4.0 {
         didSet {
@@ -145,7 +147,7 @@ import UIKit
             label.layer.borderColor = borderColor.cgColor
         }
     }
-
+    
     /// Percentage of the middle label's width. Must be between 0 and 1. Defaults to 0.5. Be sure that it is wide enough to show the value.
     @IBInspectable public var labelWidthWeight: CGFloat = 0.5 {
         didSet {
@@ -153,21 +155,21 @@ import UIKit
             setNeedsLayout()
         }
     }
-
+    
     /// Color of the flashing animation on the buttons in case the value hit the limit.
     @IBInspectable public var limitHitAnimationColor: UIColor = UIColor(red:0.26, green:0.6, blue:0.87, alpha:1)
-
+    
     /**
-        Width of the sliding animation. When buttons clicked, the middle label does a slide animation towards to the clicked button. Defaults to 5.
-    */
+     Width of the sliding animation. When buttons clicked, the middle label does a slide animation towards to the clicked button. Defaults to 5.
+     */
     let labelSlideLength: CGFloat = 5
-
+    
     /// Duration of the sliding animation
     let labelSlideDuration = TimeInterval(0.1)
-
+    
     /// Duration of the animation when the value hits the limit.
     let limitHitAnimationDuration = TimeInterval(0.1)
-
+    
     lazy var leftButton: UIButton = {
         let button = UIButton()
         button.setTitle(self.leftButtonText, for: .normal)
@@ -179,7 +181,7 @@ import UIKit
         button.addTarget(self, action: #selector(GMStepper.buttonTouchUp), for: .touchUpOutside)
         return button
     }()
-
+    
     lazy var rightButton: UIButton = {
         let button = UIButton()
         button.setTitle(self.rightButtonText, for: .normal)
@@ -191,14 +193,14 @@ import UIKit
         button.addTarget(self, action: #selector(GMStepper.buttonTouchUp), for: .touchUpOutside)
         return button
     }()
-
+    
     lazy var label: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
         if self.showIntegerIfDoubleIsInteger && floor(self.value) == self.value {
-            label.text = String(stringInterpolationSegment: Int(self.value))
+            label.text = "\(self.keyBeforeValue)\(String(stringInterpolationSegment: Int(self.value)))"
         } else {
-            label.text = String(stringInterpolationSegment: self.value)
+            label.text = "\(self.keyBeforeValue)\(String(stringInterpolationSegment: self.value))"
         }
         label.textColor = self.labelTextColor
         label.backgroundColor = self.labelBackgroundColor
@@ -209,16 +211,16 @@ import UIKit
         label.addGestureRecognizer(panRecognizer)
         return label
     }()
-
-    var labelOriginalCenter: CGPoint!
+    
+    var labelOriginalCenter: CGPoint?
     var labelMaximumCenterX: CGFloat!
     var labelMinimumCenterX: CGFloat!
-
+    
     enum LabelPanState {
         case Stable, HitRightEdge, HitLeftEdge
     }
     var panState = LabelPanState.Stable
-
+    
     enum StepperState {
         case Stable, ShouldIncrease, ShouldDecrease
     }
@@ -256,18 +258,18 @@ import UIKit
             }
         }
     }
-
+    
     /// Timer used for autorepeat option
     var timer: Timer?
-
+    
     /** When UIStepper reaches its top speed, it alters the value with a time interval of ~0.05 sec.
-        The user pressing and holding on the stepper repeatedly:
-        - First 2.5 sec, the stepper changes the value every 0.5 sec.
-        - For the next 1.5 sec, it changes the value every 0.1 sec.
-        - Then, every 0.05 sec.
-    */
+     The user pressing and holding on the stepper repeatedly:
+     - First 2.5 sec, the stepper changes the value every 0.5 sec.
+     - For the next 1.5 sec, it changes the value every 0.1 sec.
+     - Then, every 0.05 sec.
+     */
     let timerInterval = TimeInterval(0.05)
-
+    
     /// Check the handleTimerFire: function. While it is counting the number of fires, it decreases the mod value so that the value is altered more frequently.
     var timerFireCount = 0
     var timerFireCountModulo: Int {
@@ -279,67 +281,67 @@ import UIKit
             return 10 // 0.05 sec * 10 = 0.5 sec
         }
     }
-
+    
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
-
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
     }
-
+    
     func setup() {
         addSubview(leftButton)
         addSubview(rightButton)
         addSubview(label)
-
+        
         backgroundColor = buttonsBackgroundColor
         layer.cornerRadius = cornerRadius
         clipsToBounds = true
-
+        
         NotificationCenter.default.addObserver(self, selector: #selector(GMStepper.reset), name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
     }
-
+    
     public override func layoutSubviews() {
         let buttonWidth = bounds.size.width * ((1 - labelWidthWeight) / 2)
         let labelWidth = bounds.size.width * labelWidthWeight
-
+        
         leftButton.frame = CGRect(x: 0, y: 0, width: buttonWidth, height: bounds.size.height)
         label.frame = CGRect(x: buttonWidth, y: 0, width: labelWidth, height: bounds.size.height)
         rightButton.frame = CGRect(x: labelWidth + buttonWidth, y: 0, width: buttonWidth, height: bounds.size.height)
-
+        
         labelMaximumCenterX = label.center.x + labelSlideLength
         labelMinimumCenterX = label.center.x - labelSlideLength
         labelOriginalCenter = label.center
     }
-
+    
     func updateValue() {
         if stepperState == .ShouldIncrease {
             value += stepValue
         } else if stepperState == .ShouldDecrease {
             value -= stepValue
-        }   
+        }
     }
-
+    
     deinit {
         resetTimer()
         NotificationCenter.default.removeObserver(self)
     }
-
+    
     /// Useful closure for logging the timer interval. You can call this in the timer handler to test the autorepeat option. Not used in the current implementation.
-//    lazy var printTimerGaps: () -> () = {
-//        var prevTime: CFAbsoluteTime?
-//
-//        return { _ in
-//            var now = CFAbsoluteTimeGetCurrent()
-//            if let prevTime = prevTime {
-//                print(now - prevTime)
-//            }
-//            prevTime = now
-//        }
-//    }()
+    //    lazy var printTimerGaps: () -> () = {
+    //        var prevTime: CFAbsoluteTime?
+    //
+    //        return { _ in
+    //            var now = CFAbsoluteTimeGetCurrent()
+    //            if let prevTime = prevTime {
+    //                print(now - prevTime)
+    //            }
+    //            prevTime = now
+    //        }
+    //    }()
 }
 
 // MARK: Pan Gesture
@@ -352,17 +354,17 @@ extension GMStepper {
         case .changed:
             let translation = gesture.translation(in: label)
             gesture.setTranslation(CGPoint.zero, in: label)
-
+            
             let slidingRight = gesture.velocity(in: label).x > 0
             let slidingLeft = gesture.velocity(in: label).x < 0
-
+            
             // Move the label with pan
             if slidingRight {
                 label.center.x = min(labelMaximumCenterX, label.center.x + translation.x)
             } else if slidingLeft {
                 label.center.x = max(labelMinimumCenterX, label.center.x + translation.x)
             }
-
+            
             // When the label hits the edges, increase/decrease value and change button backgrounds
             if label.center.x == labelMaximumCenterX {
                 // If not hit the right edge before, increase the value and start the timer. If already hit the edge, do nothing. Timer will handle it.
@@ -377,13 +379,13 @@ extension GMStepper {
                     stepperState = .ShouldDecrease
                     panState = .HitLeftEdge
                 }
-
+                
                 animateLimitHitIfNeeded()
             } else {
                 panState = .Stable
                 stepperState = .Stable
                 resetTimer()
-
+                
                 self.rightButton.backgroundColor = self.buttonsBackgroundColor
                 self.leftButton.backgroundColor = self.buttonsBackgroundColor
             }
@@ -393,21 +395,26 @@ extension GMStepper {
             break
         }
     }
-
+    
     func reset() {
         panState = .Stable
         stepperState = .Stable
         resetTimer()
-
+        
         leftButton.isEnabled = true
         rightButton.isEnabled = true
         label.isUserInteractionEnabled = true
-
-        UIView.animate(withDuration: self.labelSlideDuration, animations: {
-            self.label.center = self.labelOriginalCenter
-            self.rightButton.backgroundColor = self.buttonsBackgroundColor
-            self.leftButton.backgroundColor = self.buttonsBackgroundColor
-        })
+        
+        UIView.animate(withDuration: self.labelSlideDuration, animations: { [weak self] () -> Void in
+            guard let weakSelf = self else {
+                return
+            }
+            if let labelOriginalCenter = weakSelf.labelOriginalCenter {
+                self?.label.center = labelOriginalCenter
+            }
+            self?.rightButton.backgroundColor = weakSelf.buttonsBackgroundColor
+            self?.leftButton.backgroundColor = weakSelf.buttonsBackgroundColor
+            })
     }
 }
 
@@ -417,21 +424,21 @@ extension GMStepper {
         rightButton.isEnabled = false
         label.isUserInteractionEnabled = false
         resetTimer()
-
+        
         if value == minimumValue {
             animateLimitHitIfNeeded()
         } else {
             stepperState = .ShouldDecrease
             animateSlideLeft()
         }
-
+        
     }
-
+    
     func rightButtonTouchDown(button: UIButton) {
         leftButton.isEnabled = false
         label.isUserInteractionEnabled = false
         resetTimer()
-
+        
         if value == maximumValue {
             animateLimitHitIfNeeded()
         } else {
@@ -439,7 +446,7 @@ extension GMStepper {
             animateSlideRight()
         }
     }
-
+    
     func buttonTouchUp(button: UIButton) {
         reset()
     }
@@ -447,27 +454,29 @@ extension GMStepper {
 
 // MARK: Animations
 extension GMStepper {
-
+    
     func animateSlideLeft() {
         UIView.animate(withDuration: labelSlideDuration) {
             self.label.center.x -= self.labelSlideLength
         }
     }
-
+    
     func animateSlideRight() {
         UIView.animate(withDuration: labelSlideDuration) {
             self.label.center.x += self.labelSlideLength
         }
     }
-
+    
     func animateToOriginalPosition() {
         if self.label.center != self.labelOriginalCenter {
             UIView.animate(withDuration: labelSlideDuration) {
-                self.label.center = self.labelOriginalCenter
+                if let labelOriginalCenter = self.labelOriginalCenter {
+                    self.label.center = labelOriginalCenter
+                }
             }
         }
     }
-
+    
     func animateLimitHitIfNeeded() {
         if value == minimumValue {
             animateLimitHitForButton(button: leftButton)
@@ -475,7 +484,7 @@ extension GMStepper {
             animateLimitHitForButton(button: rightButton)
         }
     }
-
+    
     func animateLimitHitForButton(button: UIButton){
         UIView.animate(withDuration: limitHitAnimationDuration) {
             button.backgroundColor = self.limitHitAnimationColor
@@ -487,16 +496,16 @@ extension GMStepper {
 extension GMStepper {
     func handleTimerFire(timer: Timer) {
         timerFireCount += 1
-
+        
         if timerFireCount % timerFireCountModulo == 0 {
             updateValue()
         }
     }
-
+    
     func scheduleTimer() {
         timer = Timer.scheduledTimer(timeInterval: timerInterval, target: self, selector: #selector(GMStepper.handleTimerFire), userInfo: nil, repeats: true)
     }
-
+    
     func resetTimer() {
         if let timer = timer {
             timer.invalidate()
@@ -504,6 +513,6 @@ extension GMStepper {
             timerFireCount = 0
         }
     }
-
-
+    
+    
 }
